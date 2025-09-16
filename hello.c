ef57b77c52f3aa1ff34ae3785220c13fd87b0341
25k0012nimrah <25k0012@nu.edu.pk>
2025-09-17 01:48:07 +0500

    updated

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(){
+    int num1,num2,num3,add ; 
+    printf("enter first number");
+    scanf("%d",&num1);
+    printf("enter second number");
+    scanf("%d",&num2);
+    printf("enter third number");
+    scanf("%d",&num3);
+    add=num1+num2+num3 ,add;
+    printf("the sum of three numbers is=%d",add);
+        
+    return 0;
+}
