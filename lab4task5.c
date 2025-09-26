94889e83b176d509b60f86c09ec9888da3b27f43
k250012 nimrah <k250012@nu.edu.pk>
2025-09-26 21:30:27 +0500

    second commit

---
+++ lab4task5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double a,b,c,discriminant,root1,root2;
+
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf",&a,&b,&c);
+    discriminant=b*b-4*a*c;
+    if(discriminant>0){
+        root1=(-b+sqrt(discriminant))/(2*a);
+        root2=(-b-sqrt(discriminant))/(2*a);
+        printf("Roots are real and distinct:%.2lf,%.2lf\n",root1,root2);
+    } else{
+        if(discriminant==0){
+            root1=root2=-b/(2*a);
+            printf("Roots are real and equal:%.2lf,%.2lf\n",root1,root2);
+        } else {
+            printf("Roots are imaginary.\n");
+        }
+    }
+    return 0; 
+}
