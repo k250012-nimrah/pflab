7328b5fe32008b2fd0b80a8be5dd81a8fbbc338f
k250012 nimrah <k250012@nu.edu.pk>
2025-09-26 23:15:55 +0500

    fifth commit

--- lab4task7.c
+++ lab4task7.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
 int main() {
-    char username[20], password[20];
-
+    char username[20],password[20];
     printf("Enter username: ");
-    scanf("%s", username);
+    scanf("%s",username);
     printf("Enter password: ");
-    scanf("%s", password);
-
-    if (strcmp(username, "admin") == 0) {
-        if (strcmp(password, "1234") == 0) {
+    scanf("%s",password);
+    if (strcmp(username,"admin")==0) {
+        if (strcmp(password,"1234")==0) {
             printf("Login Successful\n");
         } else {
             printf("Incorrect Password\n");
