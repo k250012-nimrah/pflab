c6445dfd8bef0800f5d98f5e34422f89b84c9fac
k250012 nimrah <k250012@nu.edu.pk>
2025-10-19 21:15:31 +0500

    fourth commit

---
+++ task1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the number of elements to be entered");
+    scanf("%d",&n);
+    int temperature[n];
+    for(int i=0;i<n;i++){
+        printf("day %d= ",i+1);
+        scanf("%d",&temperature[i]);
+    }
+    int max=temperature[0];
+    int min=temperature[0];
+    for(int j=0;j<n;j++){
+        if(temperature[j]>max){
+            max=temperature[j];
+        }
+        if(temperature[j]<min){
+            min=temperature[j];
+        }
+    }
+    printf("coldest temperature= %dC",min);
+    printf("\nhottest temperature= %dC",max);
+}
